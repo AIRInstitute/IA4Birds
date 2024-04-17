d9efb90e738195f749acc4328f66e8f64b905f4d
ivannlucas <63802817+ivannlucas@users.noreply.github.com>
2024-04-17 14:54:15 +0200

    .

--- ia4birds-all/ai4birds-ingest-service/ai4birds_ingest_service/api/models/ingest_models.py
+++ ia4birds-all/ai4birds-ingest-service/ai4birds_ingest_service/api/models/ingest_models.py
@@ -7,3 +7,8 @@ windmap_model = api.model('WindMapModel', {
     'lon': fields.Float(description='Longitude', required=True, example=-5.5495918821608665),
     'z': fields.Integer(description='Altitude', required=True, example=50)
 }, description='Model for obtaining location data based on latitude and longitude')
+
+exclusionmap_model = api.model('ExclusionMapModel',{
+   'page': fields.Integer(description= 'Number of page', required= True, example=1),
+   'page_size': fields.Integer(description='Size of page', required=True, example=100)                           
+},description='Model for exclusion map ')
