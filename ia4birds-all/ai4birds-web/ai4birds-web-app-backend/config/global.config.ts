85e63e0bf7059a9f52984c070d62ade9f8423cd1
EnriqueRonco <enrique-1909@hotmail.com>
2024-05-06 13:22:43 +0200

    .

--- ia4birds-all/ai4birds-web/ai4birds-web-app-backend/config/global.config.ts
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-backend/config/global.config.ts
@@ -10,7 +10,7 @@ const globalConfig = {
 	expiration: Number(process.env.EXPIRATION_TIME) || 180,
 	frontendURL: process.env.FRONTEND_URL || "http://localhost:5173",
 	backendURL: process.env.BACKEND_URL || "http://localhost:3030",
-	pythonURL: process.env.PYTHON_URL || "http://ai4birds-ingest:5000/ai4birds-ingest-service/v1",
+	pythonURL: process.env.PYTHON_URL || "http://ia4birds-platform.air-institute.com:5002/ai4birds-ingest-service/v1",
 	smtp: {
 		//host: process.env.SMTP_HOST || "smtp.gmail.com",
 		host: process.env.SMTP_HOST || "gmail",
