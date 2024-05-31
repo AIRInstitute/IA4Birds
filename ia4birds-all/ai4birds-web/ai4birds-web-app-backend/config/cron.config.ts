8c89cb97f32a40a5f3adfe8d7a5b631a53c01b5b
Marcos Gómez de Quero Santos <marcgomezdequero99@gmail.com>
2024-05-31 12:55:38 +0200

    [FEAT] Cron resets every last day of the month

--- ia4birds-all/ai4birds-web/ai4birds-web-app-backend/config/cron.config.ts
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-backend/config/cron.config.ts
@@ -9,15 +9,28 @@ interface DataBird {
   recordings: any[];
 }
 
-// Programar tarea para ejecutarse una vez al mes
-cron.schedule('0 0 30 * *', async () => {
-  try {
-    const response = await axios.get<DataBird>(`${globalConfig.pythonURL}/dataBird`);
-    const dataBirdData = response.data;
 
-    await redis.set('dataBirdKey', JSON.stringify(dataBirdData));
-    console.log('Datos de dataBird actualizados en Redis');
-  } catch (error) {
-    console.error('Error actualizando datos de dataBird desde el servicio Python', error);
-  }
+// Función para determinar si hoy es el último día del mes
+function isLastDayOfMonth(): boolean {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    return tomorrow.getDate() === 1;
+}
+
+
+
+// Programar tarea para ejecutarse una vez al mes
+cron.schedule('0 0 * * *', async () => {
+    if (isLastDayOfMonth()) {
+        try {
+        const response = await axios.get<DataBird>(`${globalConfig.pythonURL}/dataBird`);
+        const dataBirdData = response.data;
+
+        await redis.set('dataBirdKey', JSON.stringify(dataBirdData));
+        console.log('Datos de dataBird actualizados en Redis');
+        } catch (error) {
+            console.error('Error actualizando datos de dataBird desde el servicio Python', error);
+        }
+    }
 });
