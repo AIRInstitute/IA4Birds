2680f16437d3ac73fb5ca6f428e039b073a4401f
Marcos Gómez de Quero Santos <marcgomezdequero99@gmail.com>
2024-06-18 10:49:01 +0200

    Update map.controller.ts

--- ia4birds-all/ai4birds-web/ai4birds-web-app-backend/controllers/map.controller.ts
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-backend/controllers/map.controller.ts
@@ -176,14 +176,14 @@ async function processZip(response: any): Promise<any[]> {
 const getWindMapData = async (req, res) => {
     try {
         // Obtener los parámetros de entrada desde la solicitud
-        const { lat, lng, z } = req.body;
+        const { lat, lon, z } = req.body;
         console.log(req.body);
 
         // Hacer la solicitud al mapa eólico ibérico
         const response = await axios.post(`${globalConfig.pythonURL}/windmap`, 
             {
                 lat: lat,
-                lng: lng,
+                lon: lon,
                 z: z
             }
         );
