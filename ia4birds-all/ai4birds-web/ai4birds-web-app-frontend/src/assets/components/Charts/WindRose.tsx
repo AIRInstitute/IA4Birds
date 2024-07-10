9d4a3c3af65610ab68ec5325b9f787bc0cfb2c1a
ZoeJimenezSor <115469171+ZoeJimenezSor@users.noreply.github.com>
2024-07-10 11:45:56 +0200

    Change in Windrose column (m/s)

--- ia4birds-all/ai4birds-web/ai4birds-web-app-frontend/src/assets/components/Charts/WindRose.tsx
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-frontend/src/assets/components/Charts/WindRose.tsx
@@ -6,26 +6,36 @@ import {Spinner} from "@nextui-org/react";
 export const WindRose = ({eolicWindMapData}) => {
    console.log("WindRoseeee",eolicWindMapData);
    const [chartDataWindRose, setChartData] = useState<any>([]);
+   const formatDataWithUnits = (data) => {
+    return data.map((item) => ({
+      ...item,
+      "0-3 m/s": item["0-3"],
+      "3-6 m/s": item["3-6"],
+      "6-9 m/s": item["6-9"],
+      "9-12 m/s": item["9-12"],
+      "12-15 m/s": item["12-15"],
+      "15-18 m/s": item["15-18"],
+      "> 18 m/s": item["> 18"],
+    }));
+  };
+
   const data = {
     columns: [
       "angle",
-      "0-3",
-      "3-6",
-      "6-9",
-      "9-12",
-      "12-15",
-      "15-18",
-      "> 18",
+      "0-3 m/s",
+      "3-6 m/s",
+      "6-9 m/s",
+      "9-12 m/s",
+      "12-15 m/s",
+      "15-18 m/s",
+      "> 18 m/s",
     ],
   };
 
   useEffect(() => {
     if (eolicWindMapData && eolicWindMapData.data.chartData) {
-      setChartData(eolicWindMapData.data.chartData
-      );
-
-      console.log("eolicWindMapData WindRose",eolicWindMapData.data.chartData)
-      console.log("setChartData",chartDataWindRose);
+      const formattedData = formatDataWithUnits(eolicWindMapData.data.chartData);
+      setChartData(formattedData);
     }
   }, [eolicWindMapData]);
 
