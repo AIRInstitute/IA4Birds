75cc24e89a311239cc01cdc5ce3c9ee2a23f5e13
pblop <pablo@pabl.eu>
2024-10-30 14:07:47 +0100

    Add SMTP client

--- ia4birds-all/ai4birds-web/ai4birds-web-app-backend/config/global.config.ts
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-backend/config/global.config.ts
@@ -1,35 +1,44 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+function toBoolean(value: string | undefined): boolean | undefined {
+    if (value == undefined) return undefined;
+
+    return value === "true";
+}
+
 const globalConfig = {
-	projectName: process.env.PROJECT_NAME || "projectName",
-	port: process.env.PORT || "3030",
-	secretKey: process.env.SECRET_KEY || "secretKey",
-	sessionSecret: process.env.SECRET_SESSION || "secretSession",
-	saltRounds: Number(process.env.SALT_ROUNDS) || 10,
-	expiration: Number(process.env.EXPIRATION_TIME) || 180,
-	frontendURL: process.env.FRONTEND_URL || "http://212.128.141.36:5173",
-	backendURL: process.env.BACKEND_URL || "http://212.128.141.36:5030",
-	pythonURL: process.env.PYTHON_URL || "http://ia4birds-platform.air-institute.com:5002/ai4birds-ingest-service/v1",
-	smtp: {
-		host: process.env.SMTP_HOST || "gmail",
-		port: Number(process.env.SMTP_PORT) || 587,
-		email: process.env.SMTP_EMAIL || "email",
-		password: process.env.SMTP_PWD || "password",
-		logger: process.env.SMTP_LOGGER || true,
-	},
-	cypher: {
-		algorithm: process.env.ENC_ALGORITHM || "aes-256-abc",
-		iv: process.env.IV_VECTOR || "iv",
-		key: process.env.CYPHER_KEY || "key",
-	},
-	ssl: {
-		cert: process.env.SSL_CERT || "cert",
-		key: process.env.SSL_KEY || "key",
-	},
-	whiteList: process.env.WHITE_LIST || [],
+    projectName: process.env.PROJECT_NAME || "projectName",
+    port: process.env.PORT || "3030",
+    secretKey: process.env.SECRET_KEY || "secretKey",
+    sessionSecret: process.env.SECRET_SESSION || "secretSession",
+    saltRounds: Number(process.env.SALT_ROUNDS) || 10,
+    expiration: Number(process.env.EXPIRATION_TIME) || 180,
+    frontendURL: process.env.FRONTEND_URL || "http://212.128.141.36:5173",
+    backendURL: process.env.BACKEND_URL || "http://212.128.141.36:5030",
+    pythonURL:
+        process.env.PYTHON_URL ||
+        "http://ia4birds-platform.air-institute.com:5002/ai4birds-ingest-service/v1",
+    smtp: {
+        host: process.env.SMTP_HOST || "gmail",
+        port: Number(process.env.SMTP_PORT) || 587,
+        secure: toBoolean(process.env.SMTP_SECURE) || false,
+        email: process.env.SMTP_EMAIL || "email",
+        password: process.env.SMTP_PWD || "password",
+        logger: toBoolean(process.env.SMTP_LOGGER) || true,
+    },
+    cypher: {
+        algorithm: process.env.ENC_ALGORITHM || "aes-256-abc",
+        iv: process.env.IV_VECTOR || "iv",
+        key: process.env.CYPHER_KEY || "key",
+    },
+    ssl: {
+        cert: process.env.SSL_CERT || "cert",
+        key: process.env.SSL_KEY || "key",
+    },
+    whiteList: process.env.WHITE_LIST || [],
 };
 
-console.log(globalConfig.whiteList)
+console.log(globalConfig.whiteList);
 
 export default globalConfig;
---
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-backend/utils/smtp/smtp.ts
@@ -0,0 +1,66 @@
+import nodemailer from "nodemailer";
+import globalConfig from "../../config/global.config";
+import responseMessages from "../messages/global.messages";
+
+import Mail from "nodemailer/lib/mailer";
+
+console.log(globalConfig);
+const transporter = nodemailer.createTransport({
+  host: globalConfig.smtp.host,
+  port: globalConfig.smtp.port,
+  secure: globalConfig.smtp.secure,
+  logger: globalConfig.smtp.logger as boolean,
+  debug: true,
+  auth: {
+    user: globalConfig.smtp.email,
+    pass: globalConfig.smtp.password,
+  },
+});
+
+function testConnection() {
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log("SMTP Error: ", error);
+    } else {
+      console.log("SMTP Connection success");
+    }
+  });
+}
+
+function sendMail(
+  mailOptions: Mail.Options,
+): Promise<{ status: number; message: string }> {
+  return new Promise((resolve, reject) => {
+    try {
+      transporter.verify(function (error, success) {
+        if (error) {
+          reject({
+            status: 500,
+            message: responseMessages[500].SMTP_VERIFY_ERROR,
+          });
+        } else {
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              reject({
+                status: 500,
+                message: responseMessages[500].SMTP_SEND_ERROR,
+              });
+            } else {
+              resolve({
+                status: 200,
+                message: responseMessages[200].SMTP_EMAIL_SENT,
+              });
+            }
+          });
+        }
+      });
+    } catch (err: any) {
+      reject({
+        status: 500,
+        message: responseMessages[500].SMTP_SEND_ERROR,
+      });
+    }
+  });
+}
+
+export { sendMail, testConnection };
