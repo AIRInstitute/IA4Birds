c341ec1194ccd0348f3f9bb8c44e6cac4047172b
Marcos Gómez de Quero Santos <marcgomezdequero99@gmail.com>
2024-11-14 14:22:33 +0100

    [WIP] Sketch for Camera's and Data's panels

--- ia4birds-all/ai4birds-web/ai4birds-web-app-frontend/src/assets/components/CameraPanelComponent.tsx
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-frontend/src/assets/components/CameraPanelComponent.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import { CustomCard } from './card/CameraPanelCard';
 
 const CameraPanelComponent = () => {
-        return (
-          <div className="camera-panel-container">
-              
-          </div>
-        );
+
+    const cameraPanelData = [
+    { id: 1, name: 'Camera 1', location: '', views: '23 views', url: '/public/Buitre negro.jpg'},
+    { id: 2, name: 'Camera 2', location: '', views: '100 views', url: '/public/Buitre negro.jpg'},
+    { id: 3, name: 'Camera 3', location: '', views: '50 views', url: '/public/Buitre negro.jpg'},
+    
+    ];
+
+    return (
+        <div className="camera-panel-container">
+            <div className="card-container flex-grow mx-5">
+                <CustomCard cameraPanelData={cameraPanelData} />
+            </div>
+        </div>
+    );
 };
     
 
--- ia4birds-all/ai4birds-web/ai4birds-web-app-frontend/src/assets/components/DataPanelComponent.tsx
+++ ia4birds-all/ai4birds-web/ai4birds-web-app-frontend/src/assets/components/DataPanelComponent.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import { CustomCard } from './card/DataPanelCard';
 
 const DataPanelComponent = () => {
+
+    const dataPanelData = [
+        { id: 1, name: 'Xenocanto', data: 'Aquí van los datos de Xenocanto'},
+        { id: 2, name: 'eBird', data: 'Aquí van los datos de eBird'},
+    ];
+
+
     return (
         <div className="data-panel-container">
-            
+            <div className="card-container flex-grow mx-5">
+                <CustomCard dataPanelData={dataPanelData} />
+            </div>
         </div>
       );
 };
