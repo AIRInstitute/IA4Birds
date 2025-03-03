04766bee41488c8c3a173998e319e60d8d6f47d7
luengor <aluengor0001@gmail.com>
2025-03-03 22:41:27 +0100

    remove sched.py

--- ia4birds-all/ai4birds-ingest-service/ai4birds_celery/app.py
+++ ia4birds-all/ai4birds-ingest-service/ai4birds_celery/app.py
@@ -1,6 +1,7 @@
 from celery import Celery
 from celery.schedules import crontab
 from os import environ
+import sys
 
 # Aplicación de Celery utilizando el broker de redis
 # (que se encuentra en el contenedor llamado "redis")
@@ -11,3 +12,35 @@ celery = Celery(
     broker=f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:6379/0",
     include=['ai4birds_celery.tasks']
 )
+
+celery.conf.update(
+    task_serializer = 'json',
+    timezone = 'UTC',
+    beat_schedule = {
+        'Extraer datos de eBird y Xenocanto': {
+            'task': 'ai4birds_celery.tasks.extract',
+            'schedule': crontab(),
+        }
+    },
+)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+
+    if len(args) > 0:
+            command = args[0]
+            remaining_args = args[1:]
+            
+            if command == 'worker':
+                # For worker mode
+                celery.worker_main(['worker'] + remaining_args)
+            elif command == 'beat':
+                # For beat mode
+                celery.Beat(app=celery).run()
+            else:
+                print(f"Unknown command: {command}")
+                sys.exit(1)
+    else:
+        print("No command specified. Use 'worker' or 'beat'.")
+        sys.exit(1)
+
