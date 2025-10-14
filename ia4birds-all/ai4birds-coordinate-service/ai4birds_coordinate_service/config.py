494044605b995a41fb00cf1b47c29f5e818a8060
Alejandro Flores <aflores@air-institute.com>
2025-10-14 11:45:53 +0200

    Update config

--- ia4birds-all/ai4birds-coordinate-service/ai4birds_coordinate_service/config.py
+++ ia4birds-all/ai4birds-coordinate-service/ai4birds_coordinate_service/config.py
@@ -13,7 +13,7 @@ HOST = '0.0.0.0'
 URL_PREFIX = '/ai4birds-coordinate-service/v1'
 DEBUG_MODE = True
 
-#URL_INGEST = os.getenv('PYTHON_INGEST_URL')
-URL_INGEST = 'http://localhost:5000/ai4birds-ingest-service/v1'
+URL_INGEST = os.getenv('PYTHON_INGEST_URL')
+#URL_INGEST = 'http://localhost:5000/ai4birds-ingest-service/v1'
 
 SECRET_KEY ="secretKey"
